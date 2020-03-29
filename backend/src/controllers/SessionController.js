16a121477978b8794f9a68cc8179fff6e123585f
jaimerodriguess <jaimerodriguess@policiamilitar.sp.gov.br>
2020-03-29 10:49:10 -0300

    Login

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,18 +1,18 @@
 const connection = require('../database/connection');
 
 module.exports = { 
-    async create(Request, Response) {
-        const { id } = Request.body;
-
+    async create(request, response) {
+        const { id } = request.body;
+        
         const ong = await connection('ongs')
             .where('id', id)
             .select('name')
             .first();
 
         if(!ong) {
-            return Response.status(400).json({ error: 'No ONG found with this ID.'});
+            return response.status(400).json({ error: 'No ONG found with this ID.'});
         }
-        
-        return Response.json(ong);
+
+        return response.json(ong);
     }
 }
