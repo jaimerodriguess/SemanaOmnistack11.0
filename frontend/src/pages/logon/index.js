f29bc97327e6cf14a109b7e5dc4c1def2da9131c
jaimerodriguess <jaimerodriguess@policiamilitar.sp.gov.br>
2020-03-30 12:20:14 -0300

    ListCase

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './styles.css';
+import './style.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 import {FiLogIn} from 'react-icons/fi';
