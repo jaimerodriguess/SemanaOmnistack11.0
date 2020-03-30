16bb27d5028110510faa96f6dbf7a35078980b62
jaimerodriguess <jaimerodriguess@policiamilitar.sp.gov.br>
2020-03-30 09:37:24 -0300

    Frontend Login/Cadastro

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
@@ -13,10 +14,10 @@ export default function Logon(){
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID"/>
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={18} color="#E02041" />
                         Não tenho cadastro.
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={heroesImg} alt="heroes" />
